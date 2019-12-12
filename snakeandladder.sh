bfdfbd43e3488e3406c2b78fd2e945350788696f
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-12 16:39:45 +0530

    Added: Generated Moves for Playing Game

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -6,6 +6,16 @@ echo "Welcome to Snake and Ladder Game!"
 position=0
 
 diceResult=$(( RANDOM%6+1 ))
-echo $diceResult
+moves=$(( RANDOM%3+1 ))
 
+case $moves in
+		1)
+			playerPosition=$(( $position )) ;;
+		2)
+			playerPosition=$(( $position + $diceResult )) ;;
+		3)
+			playerPosition=$(( $position - $diceResult )) ;;
+esac
+
+echo $playerPosition
 
