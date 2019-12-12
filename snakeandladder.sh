74f3f6b163246f8f99f330c776ade03e20b0445a
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-12 17:04:06 +0530

    Added: Traversed last pos and Restart if pos less than  0

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -4,18 +4,26 @@ echo "Welcome to Snake and Ladder Game!"
 
 #variable
 position=0
+playerPosition=0
 
-diceResult=$(( RANDOM%6+1 ))
-moves=$(( RANDOM%3+1 ))
+while [ $position -lt 100 ]
+do
+	diceResult=$(( RANDOM%6+1 ))
+	moves=$(( RANDOM%3+1 ))
+	case $moves in
+			1)
+				position=$(( $position )) ;;
+			2)
+				position=$(( $position + $diceResult )) ;;
+			3)
+				position=$(( $position - $diceResult ))
+				if [ $position -lt 0 ]
+				then
+						position=0
+				fi
+ 				;;
+	esac
 
-case $moves in
-		1)
-			playerPosition=$(( $position )) ;;
-		2)
-			playerPosition=$(( $position + $diceResult )) ;;
-		3)
-			playerPosition=$(( $position - $diceResult )) ;;
-esac
-
-echo $playerPosition
+echo $position
+done
 
