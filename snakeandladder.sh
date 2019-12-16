ccdd8c5b7452440775934055f1e6e5885bc13391
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-16 16:29:50 +0530

    Added: Two players game functionality and reporting which won

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -1,17 +1,25 @@
+
 #!/bin/bash/ -x
 
 echo "Welcome to Snake and Ladder Game!"
 
 #CONSTANT
+START_POSITION=0
+WIN_POSITION=100
 NO_PLAY=1
 LADDER_MOVE=2
 SNAKE_MOVE=3
 
 #variable
+player=1
 position=0
 diceCount=0
-while [ $position -lt 100 ]
-do
+playerOne=0
+playerTwo=0
+
+function rollingDice()
+{
+	position=$1
 	diceResult=$(( RANDOM%6+1 ))
 	diceCount=$(( $diceCount+1 ))
 	getMove=$(( RANDOM%3+1 ))
@@ -21,20 +29,46 @@ do
 
 		$LADDER_MOVE)
 			position=$(( $position + $diceResult ))
-				if [ $position -gt 100 ]
-				then
-					position=$(( $position - $diceResult ))
-				fi
-				;;
+			if [ $position -gt 100 ]
+			then
+				position=$(( $position - $diceResult ))
+			fi
+			;;
 
 		$SNAKE_MOVE)
 			position=$(( $position - $diceResult ))
-				if [ $position -lt 0 ]
-				then
-					position=0
-				fi
- 				;;
+			if [ $position -lt 0 ]
+			then
+				position=0
+			fi
+ 			;;
 	esac
-echo "$position-->$diceCount"
+echo "$position $count"
+}
+
+function checkingWhoWins()
+{
+	while [ $playerOne -lt $WIN_POSITION ] && [ $playerTwo -lt $WIN_POSITION ]
+	do
+		if [ $player -eq 1 ]
+		then
+			playerOne=$( rollingDice $playerOne )
+			position=$playerOne
+			player=2
+		else
+			playerTwo=$( rollingDice $playerTwo )
+			position=$playerTwo
+			player=1
+		fi
 done
+
+	if [ $playerOne -eq $WIN_POSITION ]
+	then
+		echo "playerOne is Winner!!"
+	else
+		echo "playerTwo is Winner!!"
+	fi
+}
+checkingWhoWins
+
 
